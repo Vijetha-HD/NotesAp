9689be193317f8c415442701efdb0a3a50fae1c6
Vijetha-HD <vijethahdvijetha73672@gmail.com>
2025-07-25 10:53:00 +0530

    Build React frontend and prepare for Azure deployment

--- server/app.js
+++ server/app.js
@@ -8,28 +8,25 @@ const cors = require("cors");
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 app.use(cors());
 
+// API routes
 app.use(noteRoutes);
 
-app.use("*", (req, res) => {
-  res.status(404).json({
-    success: false,
-    errors: [
-      {
-        msg: "Route not found",
-      },
-    ],
-  });
+// Serve React frontend (after building React app into `client/build`)
+app.use(express.static(path.join(__dirname, "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
+// Error middleware
 app.use(errorMiddleware);
 
 const PORT = process.env.PORT || 3001;
-connectToDatabase().then((_) => {
-  app.listen(PORT, (_) => {
+connectToDatabase().then(() => {
+  app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
-    console.log(app.get('env')); 
+    console.log(app.get("env"));
   });
 });
